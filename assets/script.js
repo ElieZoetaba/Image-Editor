c3b96fd4cbf64df050b584e2cd29b46f9427e597
ElieZoetaba <zoetabaelie@gmail.com>
2023-01-27 23:38:55 +0000

    const rotateLeftButton
    Signed-off-by: ElieZoetaba <zoetabaelie@gmail.com>

--- assets/script.js
+++ assets/script.js
@@ -3,3 +3,4 @@ let image = document.getElementById("image");
 let downloadButton = document.getElementById("download");
 let aspectRatioBtns = document.querySelectorAll(".aspect-ratio-btns");
 const rotateRightButton = document.getElementById("rotate-right");
+const rotateLeftButton = document.getElementById("rotate-left");
